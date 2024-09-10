68104989e89df28a2d941ebb5953b72a077ed2eb
xuan0425 <54366259+xuan0425@users.noreply.github.com>
2024-09-10 19:07:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
 
+    print(f"Received request body: {body}")
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -35,7 +37,8 @@ def handle_text_message(event):
     global GROUP_ID
     user_message = event.message.text
 
-    # 檢查是否為設定群組的指令
+    print(f"Received message: {user_message} from {event.source.type}")
+
     if user_message.startswith('/設定群組'):
         if event.source.type == 'group':
             GROUP_ID = event.source.group_id
@@ -52,6 +55,7 @@ def handle_text_message(event):
     else:
         # 忽略其他文字訊息
         pass
+
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
